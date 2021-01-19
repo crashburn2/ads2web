e403b4e3e70060d4e8d7b007555bee18ffec7c1b
Petri <Petri@automation-gmbh.com>
2021-01-19 16:27:34 +0100

    Message Service implementet

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;  // Warum muss diese Variable nicht initialisiert werden mit einer default variable?
+  heroes: Hero[] = [];
   hero: Hero = {
     id: 1,
     name: 'Windfurz'
@@ -19,20 +20,29 @@ export class HeroesComponent implements OnInit {
     name: 'Windfurz'
   };
   name1: string = `${this.hero.name} ${this.hero.id}`;
-  //selectedHero: Hero; Warum geht das nicht???????????????????????????
+  //selectedHero: Hero; //Warum geht das nicht???????????????????????????
 
   //___________________
   // Ab Hier Funktionen
   //___________________
 
   onSelect(hero: Hero): void {
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}')
     this.selectedHero = hero
     console.log(this.selectedHero)
   }
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 
