7f69305b3f2f6ccd8ca899de2637aec43b29fde0
Petri <Petri@automation-gmbh.com>
2021-01-19 14:23:01 +0100

    hero details eingefügt

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit , Input} from '@angular/core';
 import{Hero} from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,12 +8,9 @@ import{Hero} from '../hero'
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero : Hero = {  //Warum kann ich hier kein element erzeugen, ohne es zu initialisieren
-    id: 1,
-    name: 'Windfurz'
-  };
+  heroes = HEROES;
+  @Input() hero : Hero = this.heroes[1];
   constructor() { }
-
 
   ngOnInit(): void {
   }
