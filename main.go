32f80b72cec75b6e87d09af8738fea62ab5b2c7f
CrashBurn <CrashBurn@DeutschePost.de>
2021-01-23 12:33:05 +0100

    go reade, now implement angular

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+//package http
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/http"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	fiber := fiber.New()
+
+	fiber.Get("/foo", func(c *fiber.Ctx) error {
+		fmt.Print("Go1")
+		return c.SendString("Mach mal was")
+	})
+
 	fmt.Println("Hello, World!")
+	fmt.Println(http.Hello("Crash"))
+
+	log.Fatal(fiber.Listen(":3000"))
 }
